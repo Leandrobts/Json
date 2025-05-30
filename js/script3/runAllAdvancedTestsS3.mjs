714cf9c49e2d918d4e81da74920ef1448043cfc7
Leandro1986 <kazaalite_601@hotmail.com>
2025-05-30 00:11:50 -0300

    Update runAllAdvancedTestsS3.mjs

--- js/script3/runAllAdvancedTestsS3.mjs
+++ js/script3/runAllAdvancedTestsS3.mjs
@@ -3,15 +3,15 @@ import { logS3, PAUSE_S3 } from './s3_utils.mjs';
 import { getOutputAdvancedS3, getRunBtnAdvancedS3 } from '../dom_elements.mjs';
 import { sprayAndInvestigateObjectExposure } from './testRetypeOOB_AB_ViaShadowCraft.mjs';
 
-async function runButterflyCorruption_v21a_wrapper() {
-    const FNAME_RUNNER = "runButterflyCorruption_v21a_wrapper";
+async function runGetSuperArrayAndAttemptAddrof_v22a_wrapper() {
+    const FNAME_RUNNER = "runGetSuperArrayAndAttemptAddrof_v22a_wrapper";
     logS3(`==== INICIANDO Estratégia Wrapper: ${FNAME_RUNNER} ====`, 'test', FNAME_RUNNER);
     await sprayAndInvestigateObjectExposure(); 
     logS3(`==== Estratégia Wrapper ${FNAME_RUNNER} CONCLUÍDA ====`, 'test', FNAME_RUNNER);
 }
 
 export async function runAllAdvancedTestsS3() {
-    const FNAME = 'runAllAdvancedTestsS3_ButterflyCorruption_v21a';
+    const FNAME = 'runAllAdvancedTestsS3_GetSuperArrayAddrof_v22a';
     const runBtn = getRunBtnAdvancedS3();
     const outputDiv = getOutputAdvancedS3();
 
@@ -19,11 +19,11 @@ export async function runAllAdvancedTestsS3() {
     if (outputDiv) outputDiv.innerHTML = '';
 
     logS3(`==== User Agent: ${navigator.userAgent} ====`,'info', FNAME);
-    logS3(`==== INICIANDO Script 3: Tentativa de Corrupção de Butterfly (lógica v21a) ====`,'test', FNAME);
-    document.title = `Iniciando Script 3 - Butterfly Corruption v21a`;
+    logS3(`==== INICIANDO Script 3: Obter SuperArray (m_vector=0) e Tentar Addrof (lógica v22a) ====`,'test', FNAME);
+    document.title = `Iniciando Script 3 - Get SuperArray & Addrof v22a`;
 
-    await runButterflyCorruption_v21a_wrapper(); 
+    await runGetSuperArrayAndAttemptAddrof_v22a_wrapper(); 
     
-    logS3(`\n==== Script 3 CONCLUÍDO (lógica v21a) ====`,'test', FNAME);
+    logS3(`\n==== Script 3 CONCLUÍDO (lógica v22a) ====`,'test', FNAME);
     if (runBtn) runBtn.disabled = false;
 }
