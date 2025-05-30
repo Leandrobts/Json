bc933387adbcb17c9675a654e10e8ca03cf5b84d
Leandro1986 <kazaalite_601@hotmail.com>
2025-05-29 23:31:44 -0300

    Update runAllAdvancedTestsS3.mjs

--- js/script3/runAllAdvancedTestsS3.mjs
+++ js/script3/runAllAdvancedTestsS3.mjs
@@ -3,16 +3,15 @@ import { logS3, PAUSE_S3 } from './s3_utils.mjs';
 import { getOutputAdvancedS3, getRunBtnAdvancedS3 } from '../dom_elements.mjs';
 import { sprayAndInvestigateObjectExposure } from './testRetypeOOB_AB_ViaShadowCraft.mjs';
 
-async function runGetterLeakAttempt_v17a_wrapper() {
-    const FNAME_RUNNER = "runGetterLeakAttempt_v17a_wrapper";
+async function runAddrofValidationAttempt_v18a_wrapper() {
+    const FNAME_RUNNER = "runAddrofValidationAttempt_v18a_wrapper";
     logS3(`==== INICIANDO Estratégia Wrapper: ${FNAME_RUNNER} ====`, 'test', FNAME_RUNNER);
-    const results = await sprayAndInvestigateObjectExposure(); // Chama a função exportada que agora tem a lógica _v17a
+    const results = await sprayAndInvestigateObjectExposure(); 
     logS3(`==== Estratégia Wrapper ${FNAME_RUNNER} CONCLUÍDA ====`, 'test', FNAME_RUNNER);
-    // Poderia logar results aqui se desejado
 }
 
 export async function runAllAdvancedTestsS3() {
-    const FNAME = 'runAllAdvancedTestsS3_GetterLeak_v17a';
+    const FNAME = 'runAllAdvancedTestsS3_AddrofVal_v18a';
     const runBtn = getRunBtnAdvancedS3();
     const outputDiv = getOutputAdvancedS3();
 
@@ -20,13 +19,11 @@ export async function runAllAdvancedTestsS3() {
     if (outputDiv) outputDiv.innerHTML = '';
 
     logS3(`==== User Agent: ${navigator.userAgent} ====`,'info', FNAME);
-    logS3(`==== INICIANDO Script 3: Tentativa de Vazamento de Ponteiro via Getter (lógica v17a) ====`,'test', FNAME);
-    document.title = `Iniciando Script 3 - Getter Leak v17a`;
+    logS3(`==== INICIANDO Script 3: Tentativa de Validação de Addrof (lógica v18a) ====`,'test', FNAME);
+    document.title = `Iniciando Script 3 - Addrof Validation v18a`;
 
-    await runGetterLeakAttempt_v17a_wrapper(); 
+    await runAddrofValidationAttempt_v18a_wrapper(); 
     
-    logS3(`\n==== Script 3 CONCLUÍDO (lógica v17a) ====`,'test', FNAME);
+    logS3(`\n==== Script 3 CONCLUÍDO (lógica v18a) ====`,'test', FNAME);
     if (runBtn) runBtn.disabled = false;
-
-    // Título é atualizado dentro do teste principal
 }
