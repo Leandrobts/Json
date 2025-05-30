db64e41c32af5213098e04b80ba1a6b8d9170580
Leandro1986 <kazaalite_601@hotmail.com>
2025-05-29 23:42:34 -0300

    Update runAllAdvancedTestsS3.mjs

--- js/script3/runAllAdvancedTestsS3.mjs
+++ js/script3/runAllAdvancedTestsS3.mjs
@@ -3,15 +3,15 @@ import { logS3, PAUSE_S3 } from './s3_utils.mjs';
 import { getOutputAdvancedS3, getRunBtnAdvancedS3 } from '../dom_elements.mjs';
 import { sprayAndInvestigateObjectExposure } from './testRetypeOOB_AB_ViaShadowCraft.mjs';
 
-async function runAddrofValidationAttempt_v18a_wrapper() {
-    const FNAME_RUNNER = "runAddrofValidationAttempt_v18a_wrapper";
+async function runAddrofAndLeakLibBase_v19b_wrapper() {
+    const FNAME_RUNNER = "runAddrofAndLeakLibBase_v19b_wrapper";
     logS3(`==== INICIANDO Estratégia Wrapper: ${FNAME_RUNNER} ====`, 'test', FNAME_RUNNER);
     const results = await sprayAndInvestigateObjectExposure(); 
     logS3(`==== Estratégia Wrapper ${FNAME_RUNNER} CONCLUÍDA ====`, 'test', FNAME_RUNNER);
 }
 
 export async function runAllAdvancedTestsS3() {
-    const FNAME = 'runAllAdvancedTestsS3_AddrofVal_v18a';
+    const FNAME = 'runAllAdvancedTestsS3_AddrofLeakBase_v19b';
     const runBtn = getRunBtnAdvancedS3();
     const outputDiv = getOutputAdvancedS3();
 
@@ -19,11 +19,11 @@ export async function runAllAdvancedTestsS3() {
     if (outputDiv) outputDiv.innerHTML = '';
 
     logS3(`==== User Agent: ${navigator.userAgent} ====`,'info', FNAME);
-    logS3(`==== INICIANDO Script 3: Tentativa de Validação de Addrof (lógica v18a) ====`,'test', FNAME);
-    document.title = `Iniciando Script 3 - Addrof Validation v18a`;
+    logS3(`==== INICIANDO Script 3: Tentativa de Addrof e Vazamento de Base da Lib (lógica v19b) ====`,'test', FNAME);
+    document.title = `Iniciando Script 3 - Addrof & Leak Base v19b`;
 
-    await runAddrofValidationAttempt_v18a_wrapper(); 
+    await runAddrofAndLeakLibBase_v19b_wrapper(); 
     
-    logS3(`\n==== Script 3 CONCLUÍDO (lógica v18a) ====`,'test', FNAME);
+    logS3(`\n==== Script 3 CONCLUÍDO (lógica v19b) ====`,'test', FNAME);
     if (runBtn) runBtn.disabled = false;
 }
