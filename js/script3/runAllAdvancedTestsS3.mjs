05bef41062606bb491fe88b6b05a16a783ef8ec2
Leandro1986 <kazaalite_601@hotmail.com>
2025-05-30 00:51:11 -0300

    Update runAllAdvancedTestsS3.mjs

--- js/script3/runAllAdvancedTestsS3.mjs
+++ js/script3/runAllAdvancedTestsS3.mjs
@@ -3,15 +3,15 @@ import { logS3, PAUSE_S3 } from './s3_utils.mjs';
 import { getOutputAdvancedS3, getRunBtnAdvancedS3 } from '../dom_elements.mjs';
 import { sprayAndInvestigateObjectExposure } from './testRetypeOOB_AB_ViaShadowCraft.mjs';
 
-async function runSelfSuperArrayTest_v24a_wrapper() {
-    const FNAME_RUNNER = "runSelfSuperArrayTest_v24a_wrapper";
+async function runSprayABWithMetadataPlant_v25a_wrapper() {
+    const FNAME_RUNNER = "runSprayABWithMetadataPlant_v25a_wrapper";
     logS3(`==== INICIANDO Estratégia Wrapper: ${FNAME_RUNNER} ====`, 'test', FNAME_RUNNER);
     await sprayAndInvestigateObjectExposure(); 
     logS3(`==== Estratégia Wrapper ${FNAME_RUNNER} CONCLUÍDA ====`, 'test', FNAME_RUNNER);
 }
 
 export async function runAllAdvancedTestsS3() {
-    const FNAME = 'runAllAdvancedTestsS3_SelfSuperArray_v24a';
+    const FNAME = 'runAllAdvancedTestsS3_SprayABMetaPlant_v25a';
     const runBtn = getRunBtnAdvancedS3();
     const outputDiv = getOutputAdvancedS3();
 
@@ -19,11 +19,11 @@ export async function runAllAdvancedTestsS3() {
     if (outputDiv) outputDiv.innerHTML = '';
 
     logS3(`==== User Agent: ${navigator.userAgent} ====`,'info', FNAME);
-    logS3(`==== INICIANDO Script 3: Testar se oob_array_buffer_real se torna Super Array (lógica v24a) ====`,'test', FNAME);
-    document.title = `Iniciando Script 3 - Self Super Array Test v24a`;
+    logS3(`==== INICIANDO Script 3: Spray ABs, Plantar Meta ABView, Trigger, Checar ABs (lógica v25a) ====`,'test', FNAME);
+    document.title = `Iniciando Script 3 - Spray AB & MetaPlant v25a`;
 
-    await runSelfSuperArrayTest_v24a_wrapper(); 
+    await runSprayABWithMetadataPlant_v25a_wrapper(); 
     
-    logS3(`\n==== Script 3 CONCLUÍDO (lógica v24a) ====`,'test', FNAME);
+    logS3(`\n==== Script 3 CONCLUÍDO (lógica v25a) ====`,'test', FNAME);
     if (runBtn) runBtn.disabled = false;
 }
